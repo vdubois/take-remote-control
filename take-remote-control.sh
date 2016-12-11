057471751314108fdd98c4987c6a808a2a7d4150
Vincent Dubois <dubois.vct@free.fr>
2016-12-11 22:51:02 +0100

    Ajout du script et écriture de la documentation

---
+++ take-remote-control.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Parametrer les adresses IP ainsi que les identifiants souris et clavier
+declare -a adresses_ip=("192.168.1.13" "192.168.1.25" "192.168.1.30")
+ID_PRINCIPAL_SOURIS=2
+ID_SECONDAIRE_SOURIS=12
+ID_PRINCIPAL_CLAVIER=3
+ID_SECONDAIRE_CLAVIER=11
+
+function disable_all()
+{
+  for i in "${adresses_ip[@]}"
+  do
+    ssh root@$i './disable.sh $ID_PRINCIPAL_SOURIS $ID_SECONDAIRE_SOURIS $ID_PRINCIPAL_CLAVIER $ID_SECONDAIRE_CLAVIER'
+  done
+}
+
+function enable_all()
+{
+  for i in "${adresses_ip[@]}"
+  do
+    ssh root@$i './enable.sh $ID_PRINCIPAL_SOURIS $ID_SECONDAIRE_SOURIS $ID_PRINCIPAL_CLAVIER $ID_SECONDAIRE_CLAVIER'
+  done
+}
+
+if [ -z "$1" ]
+  then
+    echo "ERREUR : Vous devez lancer le script avec le parametre 'enable' ou 'disable'"
+    exit
+fi
+
+if [ "$1" == "disable" ]
+  then
+    disable_all
+fi
+
+if [ "$1" == "enable" ]
+  then
+    enable_all
+fi
